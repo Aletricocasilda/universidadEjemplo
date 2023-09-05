253a5c03f59a32a0b44863f4df49590264ae4ebc
chivito362 <115670711+chivito362@users.noreply.github.com>
2023-09-04 22:17:05 -0300

    cambio agregar alumno solo label

--- src/main/java/com/am/universidadejemplo/gui/agregarAlumnos.java
+++ src/main/java/com/am/universidadejemplo/gui/agregarAlumnos.java
@@ -57,7 +57,7 @@ public class agregarAlumnos extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("CRUD Alumnos");
+        jLabel1.setText("Carga Alumnos");
 
         jLabel2.setText("Nombre");
 
